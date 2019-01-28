eabefaf6202d0aee1ca867cd69049a2487f6bd98
Luis Zabala <luis.zabala076@gmail.com>
2019-01-28 14:36:15 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+
+class Anagram
+  
+  attr_accessor :words
+  
+  def initialize(words)
+    @words = words
+  end
+  
+ def reorganize_words(words)
+   words.char.sort == @words.char.sort
+ end
+ end
+ 
+ def match(words)
+   words.select do |word|
+     reorganize_words
+   end
+ end
